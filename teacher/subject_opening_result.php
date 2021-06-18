5f330caec8284d630f61731411958f5af1a29cc6
Jeongjunseo <78674565+Jeongjunseo@users.noreply.github.com>
2021-06-18 16:05:27 +0900

    Create subject_opening_result.php

---
+++ teacher/subject_opening_result.php
@@ -0,0 +1,29 @@
+<?php
+   $con=mysqli_connect("localhost", "root", "password", "학원관리시스템") or die("MySQL 접속 실패!");
+
+   $학원이름 = $_POST["학원이름"];
+   $과목이름 = $_POST["과목이름"];
+   $과목소개 = $_POST["과목소개"];
+   $선생님아이디 = $_POST["선생님아이디"];
+   $가격 = $_POST["가격"];
+   $교재 = $_POST["교재"];
+   $수업시간 = $_POST["수업시간"];
+   $정원 = $_POST["정원"];
+
+     $sql =" INSERT INTO 과목 VALUES(NULL,'".$학원이름."','".$과목이름;
+    $sql = $sql."','".$과목소개."','".$선생님아이디."','".$가격."','".$교재."','".$수업시간."',".$정원.")";
+
+   $ret = mysqli_query($con, $sql);
+
+    echo "<h1> 신규 과목 입력 결과 </h1>";
+   if($ret) {
+           echo "데이터가 성공적으로 입력됨.";
+   }
+   else {
+           echo "데이터 입력 실패!"."<br>";
+           echo "실패 원인 :".mysqli_error($con);
+   }
+   mysqli_close($con);
+
+   echo "<br> <a href='../teacher.html'> <--뒤로 가기</a> ";
+?>
