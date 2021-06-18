bc464cc6d02ecfacff96d158b7e839f81eb2ff0e
Jeongjunseo <78674565+Jeongjunseo@users.noreply.github.com>
2021-06-18 15:55:26 +0900

    Create student_delete.php

---
+++ student/student_delete.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+<body>
+     <h1> 탈퇴 </h1>
+     <FORM METHOD="post" ACTION="student_delete_result.php">
+        탈퇴하시면 이용중인 서비스가 종료되며, 모든 데이터는 복구 불가능합니다.<br><br>
+        <fieldset>
+        <legend>입력사항</legend>
+        <table>
+                <tr>
+                        <td> 아이디</td>
+                        <td><INPUT TYPE="text" NAME="아이디"> <br></td>
+                </tr>
+                <tr>
+                        <td><INPUT TYPE="submit" VALUE="탈퇴하기"></td>
+                </tr>
+        </table>
+     </FORM>
+        <br> <a href='../student.html'> <-- 뒤로 가기 </a>
+ </body>
+ </html>
